b1ad6e9fa4bcbd95dde5a967492f74e7085a25ba
Vyacheslav <112700264+rel1se@users.noreply.github.com>
2023-04-18 22:38:13 +0300

    Add files via upload

---
+++ practice9/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("\n\n\n");
+
+		for (int i = 0; i < 13; i++) {
+			System.out.println("Hello World! ");
+		}
+
+		System.out.println("\n\n\n");
+	}
+}
