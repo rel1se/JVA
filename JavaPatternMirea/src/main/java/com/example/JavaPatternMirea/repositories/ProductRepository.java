967a85849c7e11ab5b322ab714d4372b0210ba20
Vyacheslav <112700264+rel1se@users.noreply.github.com>
2023-05-30 18:48:52 +0300

    Delete JavaPatternMirea directory

--- JavaPatternMirea/src/main/java/com/example/JavaPatternMirea/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.JavaPatternMirea.repositories;
-
-import com.example.JavaPatternMirea.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findByNameAndPrice(String name, int price);
-    boolean existsByNameAndPrice(String name, int price);
-}
